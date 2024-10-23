63131f805b1728e27e64c41366e456c8579ef011
Lirik <k.gerashhenko@gmail.com>
2024-10-23 16:52:33 +0700

    Создал два новых файла для одного и того же. Для планомерного анализа работы кода на Julia. Кстати!! блокнот хороош работает с Julia

--- MyTest/case1_analis.jl
+++ MyTest/case1_analis.jl
@@ -1,5 +1,3 @@
-GC.gc()
-
 using OrdinaryDiffEq, Flux, Optim, Random, Plots
 using Zygote
 using ForwardDiff
@@ -44,22 +42,40 @@ ub = 1.f1;
 
 # Generate data sets
 u0_list = rand(Float32, (n_exp, ns));
-display(u0_list)
 u0_list[:, 1:2] .+= 2.f-1;
 u0_list[:, 3:end] .= 0.f0;
-println()
-display(u0_list)
 
-tspan = Float32[0.0, datasize * tstep];
-println("tspan:")
-println(tspan)
+# В результате u0_list - матрица ns столбцов и n_exp строк, в которой 3+ столбцы нули
 
-tsteps = range(tspan[1], tspan[2], length=datasize);
-println("tsteps:")
-println(tsteps)
 
-ode_data_list = zeros(Float32, (n_exp, ns, datasize));
-# println("ode_data_list:")
-# display(ode_data_list)
+tspan = Float32[0.0, datasize * tstep];                           # одномерный массив с двумя элементами
+tsteps = range(tspan[1], tspan[2], length=datasize);              # последовательность от [1] до [2] с шагом length
+ode_data_list = zeros(Float32, (n_exp, ns, datasize));            # трехмерный массив нулей. Для будущего хранения решений ODE
 
 std_list = [];
+
+function max_min(ode_data)
+    return maximum(ode_data, dims=2) .- minimum(ode_data, dims=2) .+ lb
+end
+
+function trueODEfunc(dydt, y, k, t)
+    dydt[1] = -2 * k[1] * y[1]^2 - k[2] * y[1];
+    dydt[2] = k[1] * y[1]^2 - k[4] * y[2] * y[4];
+    dydt[3] = k[2] * y[1] - k[3] * y[3];
+    dydt[4] = k[3] * y[3] - k[4] * y[2] * y[4];
+    dydt[5] = k[4] * y[2] * y[4];
+end
+
+# Дале идет цикл с решением ODE для каждой строки матрицы u0_list.
+# Решение записывается в std_list
+
+for i in 1:n_exp
+    u0 = u0_list[i, :];
+    prob_trueode = ODEProblem(trueODEfunc, u0, tspan, k);
+    ode_data = Array(solve(prob_trueode, alg, saveat=tsteps));
+    ode_data += randn(size(ode_data)) .* ode_data .* noise
+    ode_data_list[i, :, :] = ode_data
+    push!(std_list, max_min(ode_data));
+end
+
+y_std = maximum(hcat(std_list...), dims=2);
